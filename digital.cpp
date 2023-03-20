b0bde68206ab7f7e32e31714e4526089817e02bd
FurlanLucas <84166961+FurlanLucas@users.noreply.github.com>
2023-03-20 12:20:37 +0100

    Create digital
    Create "digital" class for digital resources in the library.

---
+++ digital.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "digital.h"
+
+digital::digital(int reference_, std::string autor_, std::string title_,
+  int addDate_, int year_, int totalNumber_, int dispNumber_,
+  std::string format_, int size_, std::string link_)
+  :sources{reference_,autor_,title_,addDate_,year_,totalNumber_,dispNumber_}{
+
+  format = format_; // Title of the book's collection
+  size = size_; // Editor's name;
+  link = link_; // Breaf summary
+}
+
+void digital::showData(){
+  std::cout << "This item (code " << reference << ") is a " << year <<
+  " " << format << "file by" << autor << " called \"" << title <<
+  "available in " << link << ".\n";
+};
---
+++ digital.h
@@ -0,0 +1,20 @@
+#ifndef DIGITAL_H
+#define DIGITAL_H
+
+#include <string>
+#include "sources.h"
+
+class digital : public sources {
+private:
+  std::string format; // Title of the book's collection
+  int size; // Editor's name;
+  std::string link; // Breaf summary
+
+public:
+  digital(int reference_, std::string autor_, std::string title_, int addDate_,
+    int year_, int totalNumber_, int dispNumber_, std::string format_,
+    int size, std::string link);
+  void showData();
+};
+
+#endif
